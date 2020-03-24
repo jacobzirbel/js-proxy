46ef01209f92048732cfbad56cc9e7b352ba45fc
Jacob Zirbel <jzerble@gmail.com>
2020-03-24 02:17:35 -0500

    set

--- proxy.js
+++ proxy.js
@@ -4,15 +4,19 @@ let grizzlyCount = 0;
 
 // hanlder for when property is accessed
 const proxyBears = new Proxy(bears, {
-  get: function(target, prop) {
+  get: (target, prop) => {
     if (prop === "grizzly") grizzlyCount++;
     return target[prop];
+  },
+  set: (target, prop, value) => {
+    if (["grizzly", "brown", "polar"].indexOf(prop) === -1) {
+      throw new Error("Not a known type of bear");
+    }
+    target[prop] = !value;
   }
 });
 
-proxyBears.grizzly;
-proxyBears.grizzly;
-proxyBears.grizzly;
-proxyBears.grizzly;
-console.log(proxyBears.grizzly); // true
-console.log(grizzlyCount); // 5
+//proxyBears.asdf = true; // Get error
+
+proxyBears.polar = true;
+console.log(bears.polar); // returns value altered by proxy
